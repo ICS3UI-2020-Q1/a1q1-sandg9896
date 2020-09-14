6df6dc9c98d66bf2e70f4b6b4f8caf77610accc2
sandg9896 <sandg9896@wrdsb.ca>
2020-09-14 14:27:47 +0000

    Temperature

--- Main.java
+++ Main.java
@@ -1,15 +1,29 @@
-/**
+import java.util.Scanner;
+
+/** 
  *
- * @author 
+ * @author Gavin Sandhar
  */
 public class Main {
 
   /**
-   * The method that is executed when you hit the run button.
+   * The method that is executed when you hit the run button
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
     
+    // declare a variable that will store a temperature
+   String name;
+   System.out.println("Please enter a temperature you would like to convert in degrees Celsius:");
+    
+    // Creates a Scanner used for input
+    Scanner input = new Scanner(System.in);
+     int C = input.nextInt();
+    
+    // declare and calculate the temperature
+   int temperatureF = (C*9)/5 +32;
+  
+   // Tell the user the temperature
+   System.out.println(temperatureF);
   }
 }
